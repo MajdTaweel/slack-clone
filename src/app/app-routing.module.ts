232e63bea2f97a549746e7278708cd0e0e3df37c
Majd Taweel <42496516+majdtaweel@users.noreply.github.com>
2020-12-14 15:15:55 +0200

    Creating workspace feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'workspace', loadChildren: () => import('./workspace/workspace.module').then(m => m.WorkspaceModule)}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
